298d17b95d3e8883b989fbf1bfc2678efde9ab43
vozle <vozle2@gmail.com>
2022-05-15 00:44:23 +0300

    Add date logic and more tests

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,28 +11,171 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardDeliveryTest {
-    String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    public String date(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
 
     @BeforeEach
     void setUp() {
-        Configuration.holdBrowserOpen = true;
+//        Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
     }
 
     @Test
     public void shouldTestCardDeliveryForm() {
+        String meeting = date(3);
         $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
         $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
-        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(date));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(meeting));
         $("[name=\"name\"]").setValue("Колчак Александр");
         $("[name=\"phone\"]").setValue("+79118741920");
         $("[data-test-id=\"agreement\"]").click();
         $(byText("Забронировать")).click();
         $("[data-test-id=\"notification\"]").shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void shouldTestInvalidCity() {
+        String meeting = date(3);
+        $("[placeholder=\"Город\"]").setValue("Химки");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(meeting));
+        $("[name=\"name\"]").setValue("Колчак Александр");
+        $("[name=\"phone\"]").setValue("+79118741920");
+        $("[data-test-id=\"agreement\"]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(withText("Доставка в выбранный город недоступна")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldTestNotFilledCity() {
+        String meeting = date(3);
+        $("[placeholder=\"Город\"]");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(meeting));
+        $("[name=\"name\"]").setValue("Колчак Александр");
+        $("[name=\"phone\"]").setValue("+791187419201");
+        $("[data-test-id=\"agreement\"]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(withText("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldTestInvalidDate() {
+        String meeting = date(2);
+        $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(meeting));
+        $("[name=\"name\"]").setValue("Колчак Александр");
+        $("[name=\"phone\"]").setValue("+79118741920");
+        $("[data-test-id=\"agreement\"]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(withText("Заказ на выбранную дату невозможен")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldTestInvalidDateV2() {
+        $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(LocalDate.now()));
+        $("[name=\"name\"]").setValue("Колчак Александр");
+        $("[name=\"phone\"]").setValue("+79118741920");
+        $("[data-test-id=\"agreement\"]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(withText("Неверно введена дата")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldTestInvalidName() {
+        String meeting = date(3);
+        $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(meeting));
+        $("[name=\"name\"]").setValue("Kolchak Alexander");
+        $("[name=\"phone\"]").setValue("+79118741920");
+        $("[data-test-id=\"agreement\"]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(withText("Допустимы только русские буквы")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldTestNotFilledName() {
+        String meeting = date(3);
+        $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(meeting));
+        $("[name=\"name\"]");
+        $("[name=\"phone\"]").setValue("+79118741920");
+        $("[data-test-id=\"agreement\"]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(withText("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldTestInvalidPhone() {
+        String meeting = date(3);
+        $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(meeting));
+        $("[name=\"name\"]").setValue("Колчак Александр");
+        $("[name=\"phone\"]").setValue("+791187419201");
+        $("[data-test-id=\"agreement\"]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(withText("Телефон указан неверно")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldTestNotFilledPhone() {
+        String meeting = date(3);
+        $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(meeting));
+        $("[name=\"name\"]").setValue("Колчак Александр");
+        $("[name=\"phone\"]");
+        $("[data-test-id=\"agreement\"]").click();
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(withText("Поле обязательно для заполнения")).shouldBe(visible);
+    }
+
+    @Test
+    public void shouldTestNotMarkedCheckBox() {
+        String meeting = date(3);
+        $("[placeholder=\"Город\"]").setValue("Санкт-Петербург");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[placeholder=\"Дата встречи\"]").setValue(String.valueOf(meeting));
+        $("[name=\"name\"]").setValue("Колчак Александр");
+        $("[name=\"phone\"]").setValue("+79118741920");
+        $("[data-test-id=\"agreement\"]");
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(byCssSelector("[data-test-id=\"agreement\"].input_invalid .checkbox__text"));
+    }
+
+    @Test
+    public void shouldTestNotFilledAllFields() {
+        String meeting = date(3);
+        $("[placeholder=\"Город\"]");
+        $("[placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.CONTROL, "a", Keys.DELETE));
+        $("[name=\"name\"]");
+        $("[name=\"phone\"]");
+        $("[data-test-id=\"agreement\"]");
+        $(byText("Забронировать")).click();
+        $("[data-test-id=\"notification\"]").should(Condition.not(visible));
+        $(byCssSelector("[data-test-id=\"agreement\"].input_invalid .checkbox__text"));
+        $(withText("Поле обязательно для заполнения")).shouldBe(visible);
     }
 }
